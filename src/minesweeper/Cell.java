16d2797cbcba64a6ce7dcd16cecef96619470172
Andriy5852 <andriy5852@gmail.com>
2016-04-11 22:15:52 +0200

    Add flags

--- src/minesweeper/Cell.java
+++ src/minesweeper/Cell.java
@@ -2,10 +2,12 @@ package minesweeper;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JButton;
 
-public class Cell extends JButton implements ActionListener {
+public  class Cell extends JButton implements  ActionListener, MouseListener {
 
 	private static final long serialVersionUID = 4993048163772888881L;
 	private boolean hasMine = false;
@@ -17,10 +19,12 @@ public class Cell extends JButton implements ActionListener {
 	
 	public Cell(MineField mineField, int x, int y)
 	{
+		
 		this.mineField = mineField;
 		this.x = x;
 		this.y = y;
 		this.addActionListener(this);
+		this.addMouseListener(this);
 	}
 	
 	public boolean setMine()
@@ -42,15 +46,17 @@ public class Cell extends JButton implements ActionListener {
 		int[] coords = {x, y};
 		return coords;
 	}
-
+	
+	public void openCell() {
+		mineField.openCell(this);
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e)	
 	{
-		if (!isFlagged) {
-			mineField.openCell(this);	
-		}
+	
 	}
-
+	
 	public boolean isChecked()
 	{
 		return isChecked;
@@ -68,13 +74,50 @@ public class Cell extends JButton implements ActionListener {
 
 	public void flag()
 	{
-		if (isFlagged) {
+		if (isFlagged()) {
 			setText(null);
 			isFlagged = false;
 		} else {
-			setText("F");
+			setText("►");
 			isFlagged = true;
 		}
 	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getButton() == MouseEvent.BUTTON1 && !isFlagged()){
+			openCell();
+		}
+		if(e.getButton() == MouseEvent.BUTTON3 && !isChecked){
+			flag();
+	    }
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 	
 }
