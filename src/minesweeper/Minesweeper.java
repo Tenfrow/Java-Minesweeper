abced29369859267df268604826b61bcd4ed0407
Tenfrow <vit2zremb@gmail.com>
2016-04-15 22:50:09 +0300

    Add label for timer

--- src/minesweeper/Minesweeper.java
+++ src/minesweeper/Minesweeper.java
@@ -9,9 +9,16 @@ public class Minesweeper {
         JFrame frame = new JFrame("Minesweeper");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setVisible(true);
-        frame.setLayout(new GridBagLayout());
+        GridBagLayout layout = new GridBagLayout();
+        frame.setLayout(layout);
         MineField mineField = new MineField(20, 20, 0.13);
         frame.add(mineField);
+        JLabel label = new JLabel("0");
+        label.setPreferredSize(new Dimension(50, 50));
+        GridBagConstraints c = new GridBagConstraints();
+        c.anchor = GridBagConstraints.PAGE_END;
+        c.gridy = 1;
+        frame.add(new JLabel("0"), c);
         frame.pack();
         frame.setMinimumSize(frame.getSize());
     }
