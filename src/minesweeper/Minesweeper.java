5fa76317287ecf5791a076fb4b50316a912dab92
Andriy5852 <andriy5852@gmail.com>
2016-04-15 21:40:19 +0200

    test commit

--- src/minesweeper/Minesweeper.java
+++ src/minesweeper/Minesweeper.java
@@ -3,17 +3,17 @@ package minesweeper;
 import java.awt.GridLayout;
 import javax.swing.JFrame;
 public class Minesweeper {
-	
+
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Minesweeper");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		frame.setLayout(new GridLayout(1, 1));
-		
-		MineField mineField = new MineField(10, 10, 10);
+
+		MineField mineField = new MineField(15, 15, 20);
 		frame.add(mineField);
-		
+
 		frame.setBounds(100, 100, 500, 500);
 	}
-	
+
 }
