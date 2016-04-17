f5dc287cdf040e409288b5b8f10b687d9531d3b8
Tenfrow <vit2zremb@gmail.com>
2016-04-17 19:04:00 +0300

    Fix size

--- src/minesweeper/GameWindow.java
+++ src/minesweeper/GameWindow.java
@@ -15,31 +15,32 @@ class GameWindow {
     void show() {
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         window.setVisible(true);
+        window.setResizable(false);
+
         JPanel mainPanel = new JPanel();
         mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
-        JPanel statsPanel = new JPanel();
-        //stats.setBorder();
-        GridBagLayout statsLayout = new GridBagLayout();
-        statsPanel.setLayout(statsLayout);
 
+        // todo: implement stats and restart
+        JPanel statsPanel = new JPanel();
+        GridBagLayout statsLayout = new GridBagLayout();
+        statsLayout.columnWidths = new int[]{window.getWidth() / 3, window.getWidth() / 3, window.getWidth() / 3};
+        statsPanel.setLayout(statsLayout);
         statsPanel.add(new JLabel("099"));
         JButton restart = new JButton("R");
-        restart.setPreferredSize(new Dimension(40, 40));
         statsPanel.add(restart);
         statsPanel.add(new JLabel("000"));
 
+        // mainPanel.add(statsPanel);
+
         JPanel fieldPanel = new JPanel();
         fieldPanel.setLayout(new GridBagLayout());
-        MineField mineField = new MineField(10, 10, 20);
+        MineField mineField = new MineField(15, 15, 25);
         fieldPanel.add(mineField);
 
-        statsLayout.columnWidths = new int[]{window.getWidth() / 3, window.getWidth() / 3, window.getWidth() / 3};
-        mainPanel.add(statsPanel);
         mainPanel.add(fieldPanel);
 
         window.add(mainPanel);
         window.pack();
-        window.setSize(new Dimension());
-        window.setMinimumSize(window.getSize());
+        window.setSize(new Dimension(window.getWidth() + 10, window.getHeight() + 10));
     }
 }
